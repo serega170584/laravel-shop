0a729a9789ea3ff25e6b81e634d4f927db1fb1ce
Sergey Krivobokov <serega170584@gmail.com>
2022-07-01 11:21:18 +0300

    ide helper add

--- project/database/seeders/DatabaseSeeder.php
+++ project/database/seeders/DatabaseSeeder.php
@@ -16,9 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-         \App\Models\User::factory()->create([
-             'name' => 'Test User',
-             'email' => 'test@example.com',
-         ]);
+        $user = User::where('email', 'test@example.com')->first();
+        $user->createToken('test');
     }
 }
