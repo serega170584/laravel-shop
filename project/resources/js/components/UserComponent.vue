3d143bee6e7fd6117988de4b6bba6939c6adf9d7
Sergey Krivobokov <serega170584@gmail.com>
2022-07-19 09:38:12 +0300

    ide user component fix

--- project/resources/js/components/UserComponent.vue
+++ project/resources/js/components/UserComponent.vue
@@ -1,6 +1,50 @@
 <template>
-    <div class="list-group">
-        <a href="#" class="list-group-item list-group-item-action" v-for="item in data">{{item.name}}</a>
+    <div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in data">
+                    <th scope="row">{{ ++count }}</th>
+                    <td>{{item.name}}</td>
+                    <td>{{item.email}}</td>
+                    <td>
+                        <a class="btn btn-primary" href="#" @click="edit(item.id)">Edit</a>
+                        <a class="btn btn-primary" href="#">Delete</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="modal" :class="{'d-block': showModal}" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" @click="showModal=false" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="md-form mb-5">
+                            <input type="text" class="form-control" placeholder="Name" :value="user.name" disabled>
+                        </div>
+                        <div class="md-form mb-5">
+                            <input type="email" class="form-control" placeholder="E-mail" :value="user.email" disabled>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="showModal=false">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,19 +53,35 @@ export default {
     name: "UserComponent",
     mounted() {
         console.log('Component mounted.');
+        this.data = [];
         axios
             .get('http://localhost:3900/sanctum/csrf-cookie')
             .then(response => (console.log(response)));
         axios
             .get('http://localhost:3900/api/user')
             .then(response => (this.data = response.data));
+        this.count = 0;
+    },
+    beforeUpdate() {
+        this.count = 0;
+    },
+    updated() {
+
     },
     data: function () {
         return {
-            showModal: true,
-            data: []
+            showModal: false,
+            data: [],
+            count: 0,
+            user: {}
         }
     },
+    methods: {
+        edit: function (id) {
+            this.showModal = true;
+            this.user = this.data.filter(item => item.id === id).pop();
+        }
+    }
 }
 </script>
 
